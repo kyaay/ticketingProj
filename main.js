7f37de9024c1ec109e93366a365ae3aacec05e40
KHIARA DANIELLE T RUBIA <khiara.rubia@usjr.edu.ph>
2022-11-11 17:14:34 +0800

    can change the page when clicked

--- main.js
+++ main.js
@@ -9,7 +9,15 @@ window.onload = () => {
             document.querySelector('.tabs .tab.is-active').classList.remove('is-active');
             tab_switcher.parentNode.classList.add('is-active');
 
-            console.log(page_id);
-        })
+            SwitchPage(page_id);
+        });
     }
+}
+
+function SwitchPage(page_id) {
+    const current_page = document.querySelector('.pages .page.is-active');
+    current_page.classList.remove('is-active');
+
+    const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
+    next_page.classList.add('is-active');
 }
